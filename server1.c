6394b5fe824c5ac72183fcdf3e29040ea397120c
Rustam <sypooit@gmail.com>
2025-05-13 06:48:37 +0300

    try 1

--- server1.c
+++ server1.c
@@ -1,6 +1,3 @@
-#include "myqueue.h"
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -16,13 +13,14 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <time.h>
 #include <unistd.h>
 
-#define SERVERPORT       8001
-#define BUFFSIZE         4096
-#define SOCKETERROR     -1
-#define SERVERBACKLOG   100
+#include "myqueue.h"
+
+#define SERVERPORT 8001
+#define BUFFSIZE 4096
+#define SOCKETERROR -1
+#define SERVERBACKLOG 100
 #define THREAD_POOL_SIZE 20
 
 static pid_t child_pid = 0;
@@ -42,50 +40,62 @@ int server_loop(void);
 bool ensure_single_instance(void);
 void parent_sigint(int sig);
 void child_sigterm(int sig);
-void get_timestamp(char *buf, size_t len);
-void get_resolution(char *buf);
-void get_pixel_color(int x, int y, char *buf);
 
 // Ensure only one instance via lock file (parent only)
 bool ensure_single_instance(void) {
-    lock_fd = check(open("/tmp/my_server1.lock", O_CREAT | O_RDWR, 0666), "open lockfile");
+    // printf("ensure_single_instance()\n");
+    lock_fd = check(open("/tmp/my_server1.lock", O_CREAT | O_RDWR, 0666),
+                    "open lockfile");
     if (flock(lock_fd, LOCK_EX | LOCK_NB) < 0) {
         perror("flock");
         close(lock_fd);
         return false;
     }
+    // printf("ensure_single_instance return true\n");
     return true;
 }
 
 // Parent SIGINT: stop child, cleanup
 void parent_sigint(int sig) {
+    // printf("parent_sigint() wit sig=%d\n", sig);
     if (child_pid > 0) {
         kill(child_pid, SIGTERM);
         waitpid(child_pid, NULL, 0);
     }
-    if (server_socket != -1) close(server_socket);
-    if (lock_fd != -1) close(lock_fd);
+    if (server_socket != -1)
+        close(server_socket);
+    if (lock_fd != -1)
+        close(lock_fd);
     unlink("/tmp/my_server1.lock");
     exit(EXIT_SUCCESS);
 }
 
 // Child SIGTERM: exit gracefully
 void child_sigterm(int sig) {
-    if (server_socket != -1) close(server_socket);
+    // printf("child_sigterm() with sig=%d\n", sig);
+    if (server_socket != -1)
+        close(server_socket);
     exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **argv) {
+    // printf("main() with %d arguments:\n", argc);
+    // for(int i = 0; i < argc; i++) {
+    //     printf("argument %d == %s\n", i+1, argv[i]);
+    // }
+    // printf("\n");
     signal(SIGINT, parent_sigint);
 
     // Child mode
     if (argc == 2 && strcmp(argv[1], "--child") == 0) {
+        // printf("Child mode, server_socket=%d\n", server_socket);
         signal(SIGTERM, child_sigterm);
         return server_loop();
     }
 
     // Parent mode
     if (!ensure_single_instance()) {
+        // printf("Parent mode\n");
         fprintf(stderr, "Another instance is running.\n");
         return 1;
     }
@@ -97,6 +107,7 @@ int main(int argc, char **argv) {
             exit(EXIT_FAILURE);
         }
         if (pid == 0) {
+            // printf("child process with pid=%d and pid=%d\n",pid, getpid());
             execlp(argv[0], argv[0], "--child", NULL);
             perror("execlp");
             _exit(EXIT_FAILURE);
@@ -104,7 +115,8 @@ int main(int argc, char **argv) {
         child_pid = pid;
         int status;
         waitpid(child_pid, &status, 0);
-        fprintf(stderr, "Child exited (status %d), restarting in 1s...\n", WEXITSTATUS(status));
+        fprintf(stderr, "Child exited (status %d), restarting in 1s...\n",
+                WEXITSTATUS(status));
         sleep(1);
     }
     return 0;
@@ -158,10 +170,12 @@ int server_loop(void) {
         pthread_cond_signal(&condition_var);
         pthread_mutex_unlock(&mutex);
     }
+
     return 0;
 }
 
 int check(int exp, const char *msg) {
+
     if (exp == SOCKETERROR) {
         perror(msg);
         exit(EXIT_FAILURE);
@@ -170,89 +184,44 @@ int check(int exp, const char *msg) {
 }
 
 void *thread_function(void *arg) {
-    while (1) {
+
+    while (true) {
         int *pclient;
         pthread_mutex_lock(&mutex);
         if ((pclient = dequeue()) == NULL) {
             pthread_cond_wait(&condition_var, &mutex);
+            // try again
+            pclient = dequeue();
         }
         pthread_mutex_unlock(&mutex);
+
         handle_connection(pclient);
     }
 }
 
 void *handle_connection(void *p_client_socket) {
-    printf("handle_connection()\n");
+
     int client_socket = *(int *)p_client_socket;
     char buffer[BUFFSIZE];
     ssize_t bytes_read;
     int msgsize = 0;
-    char actualpath[PATH_MAX + 1];
 
-    // Read filename
-    while ((bytes_read = read(client_socket, buffer + msgsize,
-                              sizeof(buffer) - msgsize)) > 0) {
-        msgsize += bytes_read;
-        if (msgsize > BUFFSIZE - 1 || buffer[msgsize - 1] == '\n')
-            break;
+    while(true){
+        // Read request
+        while ((bytes_read = read(client_socket, buffer + msgsize,
+                                sizeof(buffer) - msgsize)) > 0) {
+            msgsize += bytes_read;
+            if (msgsize > BUFFSIZE - 1 || buffer[msgsize - 1] == '\n')
+                break;
+        }
+        check(bytes_read, "recv");
+        buffer[msgsize - 1] = '\0';
+        printf("REQUEST: %s\n", buffer);
+
+        check((int)write(client_socket, buffer, bytes_read), "write");
+        break;
     }
-    check(bytes_read, "recv");
-    buffer[msgsize - 1] = '\0';
-    printf("REQUEST: %s\n", buffer);
-
-    // // Resolve path
-    // if (realpath(buffer, actualpath) == NULL) {
-    //     perror("realpath");
-    //     close(client_socket);
-    //     return NULL;
-    // }
-
-    // FILE *fp = fopen(actualpath, "r");
-    // if (!fp) {
-    //     perror("fopen");
-    //     close(client_socket);
-    //     return NULL;
-    // }
-
-    // // Send file contents
-    // while ((bytes_read = fread(buffer, 1, BUFFSIZE, fp)) > 0) {
-    //     printf("sending %zu bytes\n", bytes_read);
-    //     check((int)write(client_socket, buffer, bytes_read), "write");
-    // }
-
     close(client_socket);
-    // fclose(fp);
+
     return NULL;
 }
-
-
-void get_timestamp(char *buf, size_t len) {
-    time_t now = time(NULL);
-    struct tm tm;
-    localtime_r(&now, &tm);
-    strftime(buf, len, "%Y-%m-%d %H:%M:%S", &tm);
-}
-
-void get_resolution(char *buf) {
-    Display *d = XOpenDisplay(NULL);
-    if (!d) { strcpy(buf, "Error"); return; }
-    Screen *s = DefaultScreenOfDisplay(d);
-    sprintf(buf, "%dx%d", s->width, s->height);
-    XCloseDisplay(d);
-}
-
-void get_pixel_color(int x, int y, char *buf) {
-    Display *d = XOpenDisplay(NULL);
-    if (!d) { strcpy(buf, "Error"); return; }
-    Window root = RootWindow(d, DefaultScreen(d));
-    XImage *img = XGetImage(d, root, x, y, 1, 1, AllPlanes, ZPixmap);
-    if (!img) { strcpy(buf, "Error"); XCloseDisplay(d); return; }
-    unsigned long pix = XGetPixel(img, 0, 0);
-    XFree(img);
-    int r = (pix & img->red_mask) >> 16;
-    int g = (pix & img->green_mask) >> 8;
-    int b = pix & img->blue_mask;
-    sprintf(buf, "#%02X%02X%02X", r, g, b);
-    XCloseDisplay(d);
-}
-
